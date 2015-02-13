6382c1efcfe51ff22e8dfd77cf5185e19b078894
Win Htaik Aung <winhtaikaung28@hotmail.com>
2015-02-14 01:01:59 +0630

    Preferences Added with Material Tool Bar

--- app/src/main/java/com/example/android/lolikeyboard/ImePreferences.java
+++ app/src/main/java/com/example/android/lolikeyboard/ImePreferences.java
@@ -17,8 +17,19 @@
 package com.example.android.lolikeyboard;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.provider.Settings;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.android.inputmethodcommon.InputMethodSettingsFragment;
 
@@ -27,6 +38,8 @@ import com.android.inputmethodcommon.InputMethodSettingsFragment;
  * Displays the IME preferences inside the input method setting.
  */
 public class ImePreferences extends PreferenceActivity {
+    Toolbar bar;
+    static InputMethodManager mImeManager;
     @Override
     public Intent getIntent() {
         final Intent modIntent = new Intent(super.getIntent());
@@ -35,13 +48,51 @@ public class ImePreferences extends PreferenceActivity {
         return modIntent;
     }
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // We overwrite the title of the activity, as the default one is "Voice Search".
-        setTitle(R.string.settings_name);
+       // setTitle(R.string.settings_name);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+            LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+            bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.pref_with_actionbar, root, false);
+            root.addView(bar, 0); // insert at top
+        }else{
+            ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+            ListView content = (ListView) root.getChildAt(0);
+
+            root.removeAllViews();
+
+            bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.pref_with_actionbar, root, false);
+            root.addView(bar);
+
+            int height;
+            TypedValue tv = new TypedValue();
+            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+                height = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+            }else{
+                height = bar.getHeight();
+            }
+
+            content.setPadding(0, height, 0, 0);
+
+            root.addView(content);
+        }
+
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mImeManager=(InputMethodManager) getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
     }
+
+
 
     public static class Settings extends InputMethodSettingsFragment {
         @Override
@@ -52,6 +103,27 @@ public class ImePreferences extends PreferenceActivity {
 
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.ime_preferences);
+
+            Preference go_ime=(Preference) findPreference("ime_setting");
+            go_ime.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    startActivity(new Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS));
+                    return true;
+                }
+            });
+
+            Preference use_ime=(Preference) findPreference("use_ime");
+            use_ime.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    mImeManager.showInputMethodPicker();
+                    return true;
+                }
+            });
+
         }
     }
+
+
 }
